867ac331bb548d57034145a248b421cd71be74be
Stephen Kurtz <stephenjkurtz@gmail.com>
2021-06-09 21:56:10 -0400

    edits

--- public/js/login.js
+++ public/js/login.js
@@ -25,14 +25,14 @@ const loginFormHandler = async (event) => {
   const signupFormHandler = async (event) => {
     event.preventDefault();
   
-    const name = document.querySelector('#name-signup').value.trim();
+    const username = document.querySelector('#username-signup').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
   
-    if (name && email && password) {
-      const response = await fetch('/api/users', {
+    if (username && email && password) {
+      const response = await fetch('/api/users/login', {
         method: 'POST',
-        body: JSON.stringify({ name, email, password }),
+        body: JSON.stringify({ username, email, password }),
         headers: { 'Content-Type': 'application/json' },
       });
   
@@ -51,4 +51,3 @@ const loginFormHandler = async (event) => {
   document
     .querySelector('.signup-form')
     .addEventListener('submit', signupFormHandler); 
-    
