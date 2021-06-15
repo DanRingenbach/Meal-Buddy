769f4dab1ff5bfa114416c24f5b46e2fd6836448
Dan Ringenbach <danringenbach.art@gmail.com>
2021-06-15 16:00:08 -0400

    nodemailer works

--- public/js/login.js
+++ public/js/login.js
@@ -2,20 +2,20 @@ const loginFormHandler = async (event) => {
   event.preventDefault();
 
   // Collect values from the login form
-  const email = document.querySelector('#email-login').value.trim();
-  const password = document.querySelector('#password-login').value.trim();
+  const email = document.querySelector("#email-login").value.trim();
+  const password = document.querySelector("#password-login").value.trim();
 
   if (email && password) {
     // Send a POST request to the API endpoint
-    const response = await fetch('/api/users/login', {
-      method: 'POST',
+    const response = await fetch("/api/users/login", {
+      method: "POST",
       body: JSON.stringify({ email, password }),
-      headers: { 'Content-Type': 'application/json' },
+      headers: { "Content-Type": "application/json" },
     });
 
     if (response.ok) {
       // If successful, redirect the browser to the profile page
-      document.location.replace('/profile');
+      document.location.replace("/profile");
     } else {
       alert(response.statusText);
     }
@@ -25,56 +25,20 @@ const loginFormHandler = async (event) => {
 const signupFormHandler = async (event) => {
   event.preventDefault();
 
-  const username = document.querySelector('#username-signup').value.trim();
-  const email = document.querySelector('#email-signup').value.trim();
-  const password = document.querySelector('#password-signup').value.trim();
-  console.log(email)
-  async function main() {
-    // Generate test SMTP service account from ethereal.email
-    // Only needed if you don't have a real mail account for testing
-    let testAccount = await nodemailer.createTestAccount();
-
-    // create reusable transporter object using the default SMTP transport
-    let transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 587,
-      secure: false, // true for 465, false for other ports
-      auth: {
-        user:  // generated ethereal user
-        pass:  // generated ethereal password
-      },
-    });
-
-    // send mail with defined transport object
-    let info = await transporter.sendMail({
-      from: '"Meal Buddy :)" <meal.buddy7@gmail.com>', // sender address
-      to: email, // list of receivers
-      subject: "Hello ✔", // Subject line
-      text: "Hello world?", // plain text body
-      html: "<b>Hello world?</b>", // html body
-    });
-
-    console.log("Message sent: %s", info.messageId);
-    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-    // Preview only available when sending through an Ethereal account
-    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-  }
+  const username = document.querySelector("#username-signup").value.trim();
+  const email = document.querySelector("#email-signup").value.trim();
+  const password = document.querySelector("#password-signup").value.trim();
+  
 
   if (username && email && password) {
-    main().catch(console.error);
-    const response = await fetch('/api/users', {
-      method: 'POST',
+    const response = await fetch("/api/users", {
+      method: "POST",
       body: JSON.stringify({ username, email, password }),
-      headers: { 'Content-Type': 'application/json' },
-      
+      headers: { "Content-Type": "application/json" },
     });
 
     if (response.ok) {
-      document.location.replace('/profile');
-
-      
+      document.location.replace("/profile");
 
       req.session.save(() => {
         req.session.user_id = userData.id;
@@ -82,7 +46,6 @@ const signupFormHandler = async (event) => {
 
         res.status(200).json(userData);
       });
-
     } else {
       alert(response.statusText);
     }
@@ -90,35 +53,29 @@ const signupFormHandler = async (event) => {
 };
 
 document
-  .querySelector('.login-form')
-  .addEventListener('submit', loginFormHandler);
+  .querySelector(".login-form")
+  .addEventListener("submit", loginFormHandler);
 
 document
-  .querySelector('.signup-form')
-  .addEventListener('submit', signupFormHandler);
-
-
+  .querySelector(".signup-form")
+  .addEventListener("submit", signupFormHandler);
 
 // Toggle between sign in & sign up diplays.
 
 const toggleSignup = () => {
-  document
-    .getElementById('login-display').style.display = 'none';
-  document
-    .getElementById('signup-display').style.display = 'block';
-}
+  document.getElementById("login-display").style.display = "none";
+  document.getElementById("signup-display").style.display = "block";
+};
 
 const toggleSignin = () => {
-  document
-  .getElementById('login-display').style.display = 'block';
-document
-  .getElementById('signup-display').style.display = 'none';
-}
+  document.getElementById("login-display").style.display = "block";
+  document.getElementById("signup-display").style.display = "none";
+};
 
 document
-  .querySelector('#toggle-signup')
-  .addEventListener('click', toggleSignup);
+  .querySelector("#toggle-signup")
+  .addEventListener("click", toggleSignup);
 
 document
-  .querySelector('#toggle-signin')
-  .addEventListener('click', toggleSignin);
+  .querySelector("#toggle-signin")
+  .addEventListener("click", toggleSignin);
