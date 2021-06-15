d196be5e173185b97eff27125548f77300413a32
Stephen Kurtz <stephenjkurtz@gmail.com>
2021-06-14 20:57:23 -0400

    update homepage

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -18,7 +18,7 @@ router.get('/', async (req, res) => {
     const meals = mealData.map((meal) => meal.get({ plain: true }));
 
     // Pass serialized data and session flag into template
-    res.render('homepage', { 
+    res.render('login', { 
       meals, 
       logged_in: req.session.logged_in 
     });
--- models/index.js
+++ models/index.js
@@ -1,6 +1,5 @@
 const Users = require('./users');
 const Meals = require('./meals');
-const Days = require('./days');
 
 Users.hasMany(Meals, {
     foreignKey: 'user_id'
